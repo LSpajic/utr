1dffc74672bcc5d832b807e09bc4960a98c27139
Lovro Spajic <lspajic@protonmail.com>
2025-04-09 13:30:06 +0000

    function to Remove unreachable states

--- lab2/MinDka.py
+++ lab2/MinDka.py
@@ -7,16 +7,14 @@ epsilonClosures = {}
 def main():
     lines = [line.rstrip('\n') for line in sys.stdin]
 
-    inputStringsLine = lines[0].split('|')
-    inputStrings = [s.split(',') if s else [] for s in inputStringsLine]
 
-    states_line = lines[1].split(',')
-    alphabet_line = lines[2].split(',')
-    accepting_states_line = lines[3].split(',') 
-    startState = lines[4].strip() 
+    states_line = lines[0].split(',')
+    alphabet_line = lines[1].split(',')
+    accepting_states_line = lines[2].split(',') 
+    startState = lines[3].strip() 
     
 
-    for line in lines[5:]:
+    for line in lines[4:]:
         line = line.strip()
 
         left, right = line.split('->', 1)
@@ -25,5 +23,30 @@ def main():
         current = current.strip()
 
         next_states = [s.strip() for s in right.split(',')]
-        states[(current,symbol)] = next_states
+        states[(current,symbol)] = set(next_states)
 
+    reachable_states = {startState}
+    new_states = {startState}
+
+    while (new_states != set([])):
+        temp = set([])
+        for state in new_states:
+            for transtion in alphabet_line:
+                if (state, transtion) in states.keys():
+                    temp = temp.union(states[(state, transtion)])
+            print(temp)
+
+        new_states = temp - reachable_states
+        reachable_states = reachable_states.union(new_states)
+
+    temp = set({})
+    for state in new_states:
+        for transtion in alphabet_line:
+            temp = temp.union(states[(state, transtion)])
+    new_states = temp - reachable_states
+    reachable_states = reachable_states.union(new_states)
+    print("Tu sam")
+    print(reachable_states)
+
+if(__name__ == "__main__"):
+    main()
